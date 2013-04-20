a83a11ccd8606ef156a519a1a5efff1c90bcaec6
Josh Klontz <josh.klontz@gmail.com>
2013-04-20 14:04:14 -0400

    script updates

--- scripts/computeDescriptors.sh
+++ scripts/computeDescriptors.sh
@@ -6,9 +6,11 @@ fi
 
 # Outputs Matlab compatible files
 
-br -algorithm "Open+LimitSize(128)+(Grid(15,15)+SIFTDescriptor(16))/(Grid(10,10)+SIFTDescriptor(24))/(Grid(5,5)+SIFTDescriptor(48))+CatRows+KMeans(1024)+CvtFloat+Hist(1024)" -train ~/cse802/images[step=5] -enroll ~/cse802/images "multiscale_sift_train.matrix(mat)" -enroll ~/cse802/validation "multiscale_sift_validate.matrix(mat)" -enroll ~/cse802/test "multiscale_sift_test.matrix(mat)"
+br -algorithm "Open+LimitSize(128)+RG+(RectRegions(32,32,24,24)/RectRegions(16,16,12,12)/RectRegions(8,8,6,6))+Hist(256,0,32)+Normalize(L1)+CatRows+KMeans(1024)+CvtFloat+Hist(1024)+Normalize(L1)+Center(Range)" -train ~/cse802/images[step=5] -enroll ~/cse802/images "normalized_color_train.matrix(mat)" -enroll ~/cse802/validation "normalized_color_validate.matrix(mat)" -enroll ~/cse802/test "normalized_color_test.matrix(mat)"
 
-#br -algorithm "Open+LimitSize(130)+Cvt(Gray)+Blur(1.1)+Gamma(0.2)+DoG(1,2)+ContrastEq(0.1,10)+LBP(1,2)+Crop(1,1,-1,-1)+RectRegions(16,16,8,8)+Hist(59)+CatRows+KMeans(1024)+CvtFloat+Hist(1024)+Normalize(L1)" -train ~/cse802/images[step=5] -enroll ~/cse802/images "normalized_lbp_train.matrix(mat)" -enroll ~/cse802/validation "normalized_lbp_validate.matrix(mat)" -enroll ~/cse802/test "normalized_lbp_test.matrix(mat)"
+#br -algorithm "Open+LimitSize(128)+(Grid(15,15)+SIFTDescriptor(16))/(Grid(10,10)+SIFTDescriptor(24))/(Grid(5,5)+SIFTDescriptor(48))+CatRows+KMeans(1024)+CvtFloat+Hist(1024)+Center(Range)" -train ~/cse802/images[step=5] -enroll ~/cse802/images "multiscale_sift_train.matrix(mat)" -enroll ~/cse802/validation "multiscale_sift_validate.matrix(mat)" -enroll ~/cse802/test "multiscale_sift_test.matrix(mat)"
+
+#br -algorithm "Open+LimitSize(130)+Cvt(Gray)+Blur(1.1)+Gamma(0.2)+DoG(1,2)+ContrastEq(0.1,10)+LBP(1,2)+Crop(1,1,-1,-1)+(RectRegions(32,32,24,24)/RectRegions(16,16,12,12)/RectRegions(8,8,6,6))+Hist(59)+CatRows+KMeans(1024)+CvtFloat+Hist(1024)+Normalize(L1)+Center(Range)" -train ~/cse802/images[step=5] -enroll ~/cse802/images "normalized_lbp_train.matrix(mat)" -enroll ~/cse802/validation "normalized_lbp_validate.matrix(mat)" -enroll ~/cse802/test "normalized_lbp_test.matrix(mat)"
 
 #br -algorithm "Open+LimitSize(130)+Cvt(Gray)+LBP(1,2)+Crop(1,1,-1,-1)+RectRegions(16,16,8,8)+Hist(59)+CatRows+KMeans(1024)+CvtFloat+Hist(1024)+Normalize(L1)" -train ~/cse802/images[step=5] -enroll ~/cse802/images "lbp_train.matrix(mat)" -enroll ~/cse802/validation "lbp_validate.matrix(mat)" -enroll ~/cse802/test "lbp_test.matrix(mat)"
 
